61a6ca72396a4c8db6ccaf27d30f4f9967414be7
Ruslan Bukin <br@bsdpad.com>
2019-12-09 23:11:35 +0000

    Work in progress on setting advertising parameters.

---
+++ hci.h
@@ -0,0 +1,26 @@
+/* 5.4.1 HCI Command Packet */
+
+/* OpCode Group Field (OGF) and OpCode Command Field (OCF). */
+
+#define	OPCODE(ogf, ocf)		(ocf | (ogf << 10))
+
+#define	OGF_LE	0x08
+
+#define	HCI_LE_SET_ADV_PARAMETERS	OPCODE(OGF_LE, 0x6)
+
+struct bt_hci_cmd {
+	uint16_t opcode;
+	uint8_t params_len;
+	void *params;
+};
+
+struct adv_param {
+	uint16_t interval_min;
+	uint16_t interval_max;
+	uint8_t adv_type;
+	uint8_t own_addr_type;
+	uint8_t peer_addr_type;
+	uint8_t peer_addr[6];
+	uint8_t channel_map;
+	uint8_t filter_policy;
+};
--- src-net/ble.c
+++ src-net/ble.c
@@ -36,10 +36,13 @@
 #include <arm/nordicsemi/nrf5340_net_core.h>
 
 #include <nrfxlib/ble_controller/include/ble_controller.h>
+#include <nrfxlib/ble_controller/include/ble_controller_hci.h>
 
 #include "ble.h"
+#include "hci.h"
 
 #define	BLE_CONTROLLER_PROCESS_IRQn	ID_EGU0
+#define	BLE_MAX_CONN_EVENT_LEN_DEFAULT	2500
 
 #define	MAX_TX_PKT_SIZE BLE_CONTROLLER_DEFAULT_TX_PACKET_SIZE
 #define	MAX_RX_PKT_SIZE BLE_CONTROLLER_DEFAULT_RX_PACKET_SIZE
@@ -66,10 +69,12 @@
 static uint8_t ble_controller_mempool[MEMPOOL_SIZE];
 
 static void
-blectlr_assertion_handler(const char *const file, const uint32_t line)
+ble_assertion_handler(const char *const file, const uint32_t line)
 {
 
-	printf("%s\n", __func__);
+	printf("%s: %s:%d\n", __func__, file, line);
+
+	while (1);
 }
 
 static void
@@ -79,11 +84,50 @@ host_signal(void)
 	printf("%s\n", __func__);
 }
 
+static void
+ble_set_adv(void)
+{
+	struct bt_hci_cmd cmd;
+	struct adv_param *adv;
+
+	adv = zalloc(sizeof(struct adv_param));
+
+	cmd.opcode = HCI_LE_SET_ADV_PARAMETERS;
+	cmd.params_len = sizeof(struct adv_param);
+	cmd.params = adv;
+
+	adv->interval_min = 0x0800;
+	adv->interval_max = 0x0800;
+	adv->adv_type = 0;
+	adv->channel_map = 7;
+
+	hci_cmd_put((uint8_t *)&cmd);
+
+	while (1);
+}
+
+static void
+print_build_rev(void)
+{
+	uint8_t ble_controller_rev[BLE_CONTROLLER_BUILD_REVISION_SIZE];
+	int i;
+
+	ble_controller_build_revision_get(ble_controller_rev);
+
+	printf("Build revision: ");
+	for (i = 0; i < BLE_CONTROLLER_BUILD_REVISION_SIZE; i++)
+		printf("%x", ble_controller_rev[i]);
+
+	printf("\n");
+}
+
 int
 ble_test(void)
 {
 	nrf_lf_clock_cfg_t clock_cfg;
 	int err;
+
+	print_build_rev();
 
 	clock_cfg.lf_clk_source = NRF_LF_CLOCK_SRC_RC;
 	clock_cfg.accuracy = NRF_LF_CLOCK_ACCURACY_500_PPM;
@@ -92,7 +136,7 @@ ble_test(void)
 
 	printf("%s: Initializing ble controller\n", __func__);
 
-	err = ble_controller_init(blectlr_assertion_handler,
+	err = ble_controller_init(ble_assertion_handler,
 	    &clock_cfg,
 	    BLE_CONTROLLER_PROCESS_IRQn);
 
@@ -103,11 +147,40 @@ ble_test(void)
 
 	printf("%s: Enabling ble controller\n", __func__);
 
+	ble_controller_cfg_t cfg;
+
+	cfg.master_count.count = BLE_MASTER_COUNT;
+	err = ble_controller_cfg_set(BLE_CONTROLLER_DEFAULT_RESOURCE_CFG_TAG,
+				     BLE_CONTROLLER_CFG_TYPE_MASTER_COUNT,
+				     &cfg);
+
+	cfg.slave_count.count = BLE_SLAVE_COUNT;
+	err = ble_controller_cfg_set(BLE_CONTROLLER_DEFAULT_RESOURCE_CFG_TAG,
+				     BLE_CONTROLLER_CFG_TYPE_SLAVE_COUNT,
+				     &cfg);
+
+	cfg.buffer_cfg.rx_packet_size = MAX_RX_PKT_SIZE;
+	cfg.buffer_cfg.tx_packet_size = MAX_TX_PKT_SIZE;
+	cfg.buffer_cfg.rx_packet_count = BLE_CONTROLLER_DEFAULT_RX_PACKET_COUNT;
+	cfg.buffer_cfg.tx_packet_count = BLE_CONTROLLER_DEFAULT_TX_PACKET_COUNT;
+
+	err = ble_controller_cfg_set(BLE_CONTROLLER_DEFAULT_RESOURCE_CFG_TAG,
+				     BLE_CONTROLLER_CFG_TYPE_BUFFER_CFG,
+				     &cfg);
+
+	cfg.event_length.event_length_us =
+		BLE_MAX_CONN_EVENT_LEN_DEFAULT;
+	err = ble_controller_cfg_set(BLE_CONTROLLER_DEFAULT_RESOURCE_CFG_TAG,
+				     BLE_CONTROLLER_CFG_TYPE_EVENT_LENGTH,
+				     &cfg);
+
 	err = ble_controller_enable(host_signal, ble_controller_mempool);
 	if (err != 0) {
 		printf("%s: controller enable returned %d\n", __func__, err);
 		return (err);
 	}
+
+	ble_set_adv();
 
 	return (0);
 }
